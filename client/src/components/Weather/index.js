b636d3d23bfc6cbb9fd77419096579e39e0d7180
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2020-06-24 04:12:43 +0200

    Materialise Weather component

--- client/src/components/Weather/index.js
+++ client/src/components/Weather/index.js
@@ -1,36 +1,90 @@
-import React from "react";
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
+import Paper from '@material-ui/core/Paper';
 
-import {
-  Grid,
-  Table,
-  TableHeaderRow,
-  ColumnChooser,
-  TableColumnVisibility,
-  Toolbar,
-} from "@devexpress/dx-react-grid-bootstrap3";
 
-class Weather extends React.Component {
-  render() {
-    return (
+export default class Weather extends React.Component {
+    state ={
+        rows: [],
+        page: 0,
+        rowsPerPage: 5
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.rowData.time !== this.props.rowData.time){
+            this.setState({rows: [...prevState.rows, this.props.rowData]})
+        }
+    }
+
+    handleChangePage = (event, newPage) => {
+        this.setState({page: newPage})
+    }
+
+    handleChangeRowsPerPage = (event) => {
+        this.setState({rowsPerPage: event.target.value ,page: 0})
+    }
+
+    render(){
+        const {rows, page, rowsPerPage} = this.state
+  return (
       <div>
-        <div className="grid-fld">
-          <Grid rows={this.props.row} columns={this.props.columns}>
-            {this.props.error && (
-              <p className="weather__error"> {this.props.error} </p>
-            )}
-
-            <Table />
-            <TableHeaderRow />
-            <TableColumnVisibility
-              defaultHiddenColumnNames={this.props.defaultHiddenColumnNames}
-            />
-            <Toolbar />
-            <ColumnChooser />
-          </Grid>
-        </div>
-      </div>
-    );
-  }
+    <TableContainer component={Paper}>
+      <Table stickyHeader size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell>Wind Direction</TableCell>
+            <TableCell>Wind Speed</TableCell>
+            <TableCell>Wind Gust</TableCell>
+            <TableCell>Wind Speed Average 2m</TableCell>
+            <TableCell>Wind Direction Average 2m</TableCell>
+            <TableCell>Wind Gust Average 10m</TableCell>
+            <TableCell>Wind Gust Direction 10m</TableCell>
+            <TableCell>Rain intensity</TableCell>
+            <TableCell>Daily raining intensity</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) =>  (
+            <TableRow key={row.time}>
+              <TableCell component="th" scope="row">
+                {row.date}
+              </TableCell>
+              <TableCell align="right">{row.time}</TableCell>
+              <TableCell align="right">{row.winddir}</TableCell>
+              <TableCell align="right">{row.windspeedmph}</TableCell>
+              <TableCell align="right">{row.windgustmph}</TableCell>
+              <TableCell align="right">{row.windgustdir}</TableCell>
+              <TableCell align="right">{row.windspdmph_avg2m}</TableCell>
+              <TableCell align="right">{row.winddir_avg2m}</TableCell>
+              <TableCell align="right">{row.windgustmph_10m}</TableCell>
+              <TableCell align="right">{row.windgustdir_10m}</TableCell>
+              <TableCell align="right">{row.rainin}</TableCell>
+              <TableCell align="right">{row.dailyrainin}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    <TablePagination
+        component={Paper}
+        rowsPerPageOptions={[5,10,15]}
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={this.handleChangePage}
+        onChangeRowsPerPage={this.handleChangeRowsPerPage}
+      />
+          </div>
+  )
+          }
 }
 
-export default Weather;
