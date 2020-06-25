e742144824db4a8e9e491d46e551e6e959be3bd8
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2020-06-25 11:03:04 +0200

    Refactoring line chart

--- client/src/components/Charts/Line/index.js
+++ client/src/components/Charts/Line/index.js
@@ -1,38 +1,37 @@
-import React from 'react'
-import useChartConfig from '../../../hooks/useChartConfig'
-import Box from '../../Box'
-import { Chart } from 'react-charts'
-import './linechart.css'
+import React from "react";
+import useChartConfig from "../../../hooks/useChartConfig";
+import Box from "../../Box";
+import { Chart } from "react-charts";
 
 export default class Line extends React.Component {
+  state = {
+    chartData: [
+      {
+        label: "Series 1",
+        data: [
+          { x: 1, y: 10 },
+          { x: 2, y: 10 },
+          { x: 3, y: 10 },
+        ],
+      },
+    ],
+    series: {
+      showPoints: true,
+    },
+    axes: [
+      { primary: true, type: "time", position: "bottom" },
+      { type: "linear", position: "left" },
+    ],
+  };
 
-    state = {
-        chartData: [{
-            label: 'Series 1',
-            data: [
-              { x: 1, y: 10 },
-              { x: 2, y: 10 },
-              { x: 3, y: 10 }
-            ],
-          }],
-        series: {
-            showPoints: true
-        },
-        axes: [
-            { primary: true, type: 'time', position: 'bottom' },
-            { type: 'linear', position: 'left' }
-          ]
-    }
-    
-
-render(){
-    const {chartData, series, axes} = this.state
-  return (
-    <div>
-      <Box resizable>
-        <Chart data={chartData} series={series} axes={axes} tooltip />
-      </Box>
-    </div>
-  )
-}
+  render() {
+    const { chartData, series, axes } = this.state;
+    return (
+      <div>
+        <Box resizable>
+          <Chart data={chartData} series={series} axes={axes} tooltip />
+        </Box>
+      </div>
+    );
+  }
 }
